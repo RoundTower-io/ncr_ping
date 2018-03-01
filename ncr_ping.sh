2856cad505d3721ecc26f314e357aeafc49c61d5
gamename <tennis.n.smith@gmail.com>
2018-03-01 12:57:58 -0600

    say what we ping

--- ncr_ping.sh
+++ ncr_ping.sh
@@ -2,11 +2,18 @@
 #
 #
 set -o errexit   # force exit when a command fails
-set -o nounset   # force exit if we use undeclared variables
 set -o pipefail  # when piping, set exit code to last non-zero in pipeline
 
 timestamp=$(date +'%Y%m%d_%H%M%S')
+
 netstat | grep ESTABLISHED | cut -d' ' -f 2 | cut -d'.' -f1-4 | while read -r ip_addr; do
-  ping_result = $(ping -s ${ip_addr} 100 3 | grep round | cut -d' ' -f6 | sed 's/\//,/g')
-  echo ${ping_result} > /tmp/${ip_addr}_${timestamp}.txt
+    echo "pinging ${ip_addr}"
+    ip_addr_dir=$(echo "${ip_addr}" | sed 's/\./_/g')
+    if ! [[ -d "/tmp/ping_tests/${ip_addr_dir}" ]]; then
+        mkdir -p "/tmp/ping_tests/${ip_addr_dir}"
+    fi
+    ping_result=$(ping -s ${ip_addr} 100 3 | grep round | cut -d' ' -f6 | sed 's/\//,/g')
+    echo ${ping_result} > "/tmp/ping_tests/${ip_addr_dir}/${timestamp}.txt"
 done
+
+tar cvf - /tmp/ping_tests | gzip -c > /tmp/ping_tests.tar.gz
