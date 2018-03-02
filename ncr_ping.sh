f52f843d33f07b35567b281a53dc55efe723403c
gamename <tennis.n.smith@gmail.com>
2018-03-02 09:22:35 -0600

    updated script documentation

--- ncr_ping.sh
+++ ncr_ping.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 #
+# A simple utility that will create 2 files based on netstat output.
 #
+# File 1 contains output based on the following
+#   1. Run netstat
+#   2. Get all endpoints that are ESTABLISHED status (i.e. the ip address of the endpoint)
+#   3. Ping the endpoint x times
+#   4. Record to a csv file the ping statistics (details below)
+#
+# File 2 contains output based on the following rules
+#   1. Run netstat
+#   2. Get all endpoints that are in 'TIME_WAIT', 'CLOSE_WAIT' or 'SYN_SENT' status
+#   3. Record the output to a csv file (details below)
 #
 set -o errexit   # force exit when a command fails
 set -o pipefail  # when piping, set exit code to last non-zero in pipeline
